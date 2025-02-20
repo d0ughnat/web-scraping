37850d113164dce33418f60f906ee69cd5f81a5a
d0ughnat <nledesma35@gmail.com>
2025-02-20 22:23:34 +0800

    change

--- practice.py
+++ practice.py
@@ -11,7 +11,7 @@ from pathlib import Path
 import tempfile
 import zipfile
 import io
-from google.oauth2.credentials import Credentials
+from google.oauth2.service_account import Credentials
 from google.oauth2 import service_account
 from googleapiclient.discovery import build
 from googleapiclient.http import MediaFileUpload
